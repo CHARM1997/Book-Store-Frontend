c3e512a8fc1569f3b2ae08baf70299361dc5d8ed
it18208436 <it18208436@my.sliit.lk>
2022-07-25 14:51:32 +0530

    frontend and backend combined

--- src/components/Book/Books.js
+++ src/components/Book/Books.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
+import axios from 'axios';
+const URL = "http://localhost:5000/books";
+const fetchHandler = async() => {
+    return await axios.get(URL).then((res)=> res.data)
+};
 const Books = () => {
+    const [Books, setBooks] = useState();
+    useEffect(() => {
+        fetchHandler().then(data=>setBooks(data))
+    }, []);
+    console.log(Books);
   return (
-    <div>Books Here</div>
+    <div>All Books are here</div>
   )
 };
 
