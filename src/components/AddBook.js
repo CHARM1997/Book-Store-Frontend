d41795f9a5d98569196382299a4b4902d3c6c7d1
it18208436 <it18208436@my.sliit.lk>
2022-07-25 13:35:03 +0530

    create routes successfully

---
+++ src/components/AddBook.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AddBook = () => {
+  return(
+    <div>AddBook From Here</div>
+  )
+};
+
+export default AddBook;
---
+++ src/components/Book/Books.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Books = () => {
+  return (
+    <div>Books Here</div>
+  )
+};
+
+export default Books;
