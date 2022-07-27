d12f6bd8557927cc418740e00b5f0c7f35b90198
it18208436 <it18208436@my.sliit.lk>
2022-07-27 07:30:33 +0530

    add book function tested successfully

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -2,26 +2,29 @@ import { FormControlLabel, Checkbox, Button, FormLabel, TextField } from '@mui/m
 import { Box } from '@mui/system';
 import React, { useState } from 'react';
 
-
 const AddBook = () => {
-  const [inputs, setinputs] = useState({
-    name:"",
-    description:"",
-    price:"",
-    author:"",
-    available:false,
-    Image:","
+  const [inputs, setInputs] = useState({
+    name: "",
+    description: "",
+    price: "",
+    author: "",
+    
+    image: "",
   });
-  const [checked, setchecked] = useState(false);
+  const [checked, setChecked] = useState(false);
   const handleChange = (e) => {
-    setinputs((prevState) => ({
+    setInputs((prevState) => ({
     ...prevState,
-    [e.target.name]: e.target.value
-  })) 
+    [e.target.name]: e.target.value,
+  }));
   /* console.log(e.target.name,"Value",e.target.value) */
 };
+const handleSubmit = (e) => {
+  e.preventDefault();
+  console.log(inputs,checked);
+};
   return(
-    <form>
+    <form onSubmit={handleSubmit}>
     <Box display="flex" 
     flexDirection="column" 
     justifyContent={"center"} 
@@ -33,15 +36,21 @@ const AddBook = () => {
     marginTop={10}>
     <FormLabel>Name</FormLabel>
     <TextField value={inputs.name} onChange={handleChange} margin ="normal" fullWidth variant ="outlined" name ="name"/>
+
     <FormLabel>Author</FormLabel>
     <TextField value={inputs.author} onChange={handleChange} margin ="normal" fullWidth variant ="outlined" name ="author"/>
+
     <FormLabel>Description</FormLabel>
     <TextField value={inputs.description} onChange={handleChange} margin ="normal" fullWidth variant ="outlined" name ="description"/>
+
     <FormLabel>Price</FormLabel>
     <TextField value={inputs.price} onChange={handleChange} type="number" margin ="normal" fullWidth variant ="outlined" name ="price"/>
+
     <FormLabel>Image</FormLabel>
-    <TextField value={inputs.Image} onChange={handleChange} margin ="normal" fullWidth variant ="outlined" name ="image"/>
-    <FormControlLabel control={<Checkbox checked={checked} />} onChange={()=>setchecked(!checked)} label="Available" />
+    <TextField value={inputs.image} onChange={handleChange} margin ="normal" fullWidth variant ="outlined" name ="image"/>
+
+    <FormControlLabel control={<Checkbox checked={checked} onChange={()=>setChecked(!checked)}/>} label="Available" />
+
     <Button variant="contained" type="submit">Add Book</Button>
     </Box>
     </form>
